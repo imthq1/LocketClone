a8480b766d5594a4c650eb6fde97da642534f8f0
Nguyen Toan Thang <xamthang09@gmail.com>
2025-10-12 16:02:03 +0700

    add domain Message, CConversation

--- SocialNetwork/src/main/java/SocialNetwork/SocialNetwork/domain/User.java
+++ SocialNetwork/src/main/java/SocialNetwork/SocialNetwork/domain/User.java
@@ -21,7 +21,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false, length = 190)
     private String email;
